8210bcf1e87e55cad623da14a9c9dcf07074c642
unknown <janreyferil@gmail.com>
2018-03-25 23:52:18 +0800

    Create factories and run the seed

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->randomFloat(2, 10, 1000),
+        'stock' => $faker->numberBetween(100, 10000),
+        'discount' => $faker->numberBetween(2, 30)
     ];
 });
