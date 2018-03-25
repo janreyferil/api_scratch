7357159f181cedec81d7f5f1054e87855938f0b8
unknown <janreyferil@gmail.com>
2018-03-26 00:12:00 +0800

    Created Relationship with tables

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -4,7 +4,11 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Model\Product;
+
 class Review extends Model
 {
-    //
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
